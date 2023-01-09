22e4afd8cd67916e5d22c153f08468499710501e
Danilo <DaniloSVJ>
2023-01-08 23:55:56 -0300

    atz

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -10,7 +10,14 @@ class CreateUserUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const usersAlreadyExists = this.usersRepository.findByEmail(email);
+
+    if (usersAlreadyExists) {
+      throw new Error("User already exists");
+    }
+    // const user = this.usersRepository.create({ email, name });
+
+    return this.usersRepository.create({ email, name });
   }
 }
 
